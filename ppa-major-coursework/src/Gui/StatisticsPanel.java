731939e9e4f0ae74830e033ccca53a5bca24b5a6
Aakash <Aakash@10.40.167.67>
2017-02-28 20:29:45 +0000

    implemented changing statistics subPanels and button listeners for said subPanels. Still have to add comments and clean up code.

--- ppa-major-coursework/src/Gui/StatisticsPanel.java
+++ ppa-major-coursework/src/Gui/StatisticsPanel.java
@@ -3,36 +3,83 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
+import javax.swing.border.EtchedBorder;
 
 public class StatisticsPanel extends JPanel {
 	
+	private StatisticsSubPanel subPanel0;
 	private StatisticsSubPanel subPanel1;
 	private StatisticsSubPanel subPanel2;
 	private StatisticsSubPanel subPanel3;
-	private StatisticsSubPanel subPanel4;
+	private String[] titles = new String[8];
+	private String[] stats = new String[8];
+	private boolean[] positionBools = {false, false, false, false, false, false, false, false};
 	
 	
 	//TODO: check if initMainWidgets() works if put above StatisticsSubPanel inner class
 	
 	StatisticsPanel(){
 		super();	
-		subPanel1 = new StatisticsSubPanel();
-		subPanel2 = new StatisticsSubPanel();
-		subPanel3 = new StatisticsSubPanel();
-		subPanel4 = new StatisticsSubPanel();
+		
+		titles[0] = "T0";
+		titles[1] = "T1";
+		titles[2] = "T2";
+		titles[3] = "T3";
+		titles[4] = "T4";
+		titles[5] = "T5";
+		titles[6] = "T6";
+		titles[7] = "T7";
+		
+		stats[0] = "S0";
+		stats[1] = "S1";
+		stats[2] = "S2";
+		stats[3] = "S3";
+		stats[4] = "S4";
+		stats[5] = "S5";
+		stats[6] = "S6";
+		stats[7] = "S7";
+		
+		subPanel0 = new StatisticsSubPanel(0);
+		subPanel1 = new StatisticsSubPanel(1);
+		subPanel2 = new StatisticsSubPanel(2);
+		subPanel3 = new StatisticsSubPanel(3);
 		initMainWidgets();
 	}
 
+	
 	public class StatisticsSubPanel extends JPanel {
 
+		private JLabel title;
+		private JLabel statistic;
+		private int position;
+		
 		StatisticsSubPanel() {
 			super();
 			initWidgets();
+			setBorder(new EtchedBorder(EtchedBorder.RAISED));
+			for (int i = 0; i < positionBools.length ; i ++){
+				if (!positionBools[i]){
+					position = i;
+					positionBools[i] = true;
+					setStatistic(i);
+					break;
+				}
+			}
+		}
+		
+		StatisticsSubPanel(int index){
+			super();
+			initWidgets();
+			position = index;
+			setStatistic(index);
+			setBorder(new EtchedBorder(EtchedBorder.RAISED));
 		}
 
 		public void initWidgets() {
@@ -43,6 +90,8 @@ public class StatisticsPanel extends JPanel {
 			Font buttonFont = new Font("Dialog", Font.BOLD, 30);
 			leftButton.setFont(buttonFont);
 			rightButton.setFont(buttonFont);
+			rightButton.addActionListener(new StatsButtonListeners(true));
+			leftButton.addActionListener(new StatsButtonListeners(false));
 			// leftButton.setBorderPainted(false);
 			// leftButton.setFocusPainted(false);
 			// leftButton.setContentAreaFilled(false);
@@ -56,8 +105,8 @@ public class StatisticsPanel extends JPanel {
 			statisticHolder.setLayout(new BorderLayout());
 			this.add(statisticHolder, BorderLayout.CENTER);
 
-			JLabel title = new JLabel("Title");
-			JLabel statistic = new JLabel("42");
+			title = new JLabel("Title");
+			statistic = new JLabel("42");
 			title.setHorizontalAlignment(SwingConstants.CENTER);
 			statistic.setHorizontalAlignment(SwingConstants.CENTER);
 
@@ -67,17 +116,60 @@ public class StatisticsPanel extends JPanel {
 			statisticHolder.add(statistic, BorderLayout.CENTER);
 
 			this.setMinimumSize(new Dimension(300, 300));
+			//find appropriate border
+			//setBorder(new B);
 		}
+		
+		public void setStatistic(int index){
+			positionBools[position] = false;
+			title.setText(titles[index]);
+			statistic.setText(stats[index]);
+			positionBools[index] = true;
+			position = index;
+			this.revalidate();
+			this.repaint();
+		}
+		
+		public void moveRight(){
+			int newPosition = position;
+			while(positionBools[newPosition]){
+				newPosition = (newPosition + 1) % 8;
+			}
+			
+			setStatistic(newPosition);
+		}
+		
+		public void moveLeft(){
+			int newPosition = position;
+			while(positionBools[newPosition]){
+				newPosition = (((newPosition - 1)%8)+8)%8;
+			}
+			setStatistic(newPosition);
+		}
+		
+		public class StatsButtonListeners implements ActionListener{
 
+			boolean goRight;
+			public StatsButtonListeners(boolean goRight) {
+				this.goRight = goRight;
+			}
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(goRight){
+					moveRight();
+				}else{
+					moveLeft();
+				}
+			}
+		}
 	}
-	
 	
 	public void initMainWidgets(){
 		setLayout(new GridLayout(2, 2));
+		add(subPanel0);
 		add(subPanel1);
 		add(subPanel2);
 		add(subPanel3);
-		add(subPanel4);
 	}
-
 }
