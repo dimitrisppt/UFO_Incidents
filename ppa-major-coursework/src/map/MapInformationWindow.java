9b539d9f4dd8d6e7c5c61ad7af3b394400137d73
Dimitris Papatheodoulou <dimitris.papatheodoulou@kcl.ac.uk>
2017-03-09 20:17:04 +0000

    Added a new class which is the pop up window containing the information of sightings.

---
+++ ppa-major-coursework/src/map/MapInformationWindow.java
@@ -0,0 +1,18 @@
+package map;
+
+import javax.swing.JFrame;
+
+import api.ripley.Ripley;
+
+public class MapInformationWindow extends JFrame{
+	
+
+	public MapInformationWindow() {
+		
+		super("");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		
+	
+	}
+}
