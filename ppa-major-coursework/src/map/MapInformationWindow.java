e4d086bff7054d55ea7d27f8a22aac2c8a42b9ce
Dimitris Papatheodoulou <dimitris.papatheodoulou@kcl.ac.uk>
2017-03-09 21:43:31 +0000

    Completed the ui of the new window

--- ppa-major-coursework/src/map/MapInformationWindow.java
+++ ppa-major-coursework/src/map/MapInformationWindow.java
@@ -1,8 +1,16 @@
 package map;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 
-import api.ripley.Ripley;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
 
 public class MapInformationWindow extends JFrame{
 	
@@ -13,6 +21,29 @@ public class MapInformationWindow extends JFrame{
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 		
+		JPanel jpTop = new JPanel();
+		JPanel jpCenter = new JPanel();
+		
+		JComboBox<String> jcbSortSightings = new JComboBox<String>();
+	
+		JButton button = new JButton("Test");
+		JTextArea jtaIncidentInfo = new JTextArea();
+		
+		JScrollPane scroll = new JScrollPane(jtaIncidentInfo);
+		scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		
+		jpTop.setLayout(new BorderLayout());
+		jpTop.add(jcbSortSightings, BorderLayout.CENTER);
+		
+		jpCenter.setLayout(new BorderLayout());
+		jpCenter.add(scroll, BorderLayout.CENTER);	
+		
+		setLayout(new BorderLayout());
+		add(jpTop, BorderLayout.NORTH);
+		add(jpCenter, BorderLayout.CENTER);
+		
+		pack();
+		setVisible(true);
 	
 	}
 }
