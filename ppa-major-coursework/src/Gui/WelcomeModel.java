53530418b98fb6b3d3300114830d9b594b7e3680
Henry Valeyre <henry.valeyre@kcl.ac.uk>
2017-03-20 22:00:39 +0000

    Finished implementing WelcomePanel. Still need to fix timing problems where text only changes after data is fetched

--- ppa-major-coursework/src/Gui/WelcomeModel.java
+++ ppa-major-coursework/src/Gui/WelcomeModel.java
@@ -3,29 +3,12 @@ package Gui;
 import java.util.Observable;
 
 public class WelcomeModel extends Observable{
-	String from;
-	String to;
 	
-	public String getfrom(){
-		return from;
-	}
 	
-	public String getTo(){
-		return to;
-	}
-	
-	public void setFrom(String from){
-		this.from=from;
-		updateObserver();	}
-	
-	public void setTo(String to){
-		this.to=to; 
-		updateObserver();
-	}
 	
 	private void updateObserver(){
 		setChanged();
 		notifyObservers();
-		
+		System.out.println("welcomeModel");
 	}
 }
