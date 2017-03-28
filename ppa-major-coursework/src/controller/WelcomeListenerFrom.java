b3e496faa8201d2a2bd3904c2790751a821d450e
Henry Valeyre <henry.valeyre@kcl.ac.uk>
2017-03-28 22:35:29 +0100

    Comments for the WelcomeListenerFrom and the WelcomeListenerTo classes

--- ppa-major-coursework/src/controller/WelcomeListenerFrom.java
+++ ppa-major-coursework/src/controller/WelcomeListenerFrom.java
@@ -1,5 +1,7 @@
 package controller;
 
+//This class is part of the controller , it will get the Date when the date od the FromDate is selected in the JComboBox and pass it to the incident fetcher 
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JComboBox;
@@ -8,16 +10,22 @@ import model.IncidentsFetcher;
 
 	public class WelcomeListenerFrom implements ActionListener {
 		
+		//We then create an object of the Incident fetcher which is the model
+
 		private IncidentsFetcher fetcher;
 		
 		public WelcomeListenerFrom(IncidentsFetcher ifetcher) {
 			fetcher = ifetcher;
+		//We then initialise the class variables and pass the incidentFetchet to the constructor
 		}
 		
 		public void actionPerformed(ActionEvent e){
+			//We then create here a temporary comboBox that will take the value of the user selection
 			JComboBox<Integer> fromComboBox = (JComboBox) e.getSource();
+			//We then create a startYear integer variable that will have the value of the Date selected 
 			int startYear = (int) fromComboBox.getItemAt(fromComboBox.getSelectedIndex());
-            fetcher.setStartDate(startYear);
+			//And we then set the startYear of the fetcher to the value of the startYear variable
+			fetcher.setStartDate(startYear);
         }
 	}
 	
