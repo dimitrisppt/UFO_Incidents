d665c1b543316ae81b3c1eb098916089ac1e147f
Aakash <k1630608@kcl.ac.uk>
2017-03-29 21:49:56 +0100

    added comments to YearWithMostIncidences and StatisticsPanel

--- ppa-major-coursework/src/statistics/model/personalStats/YearWithMostIncidents.java
+++ ppa-major-coursework/src/statistics/model/personalStats/YearWithMostIncidents.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 import api.ripley.Incident;
 import model.IncidentsFetcher;
 
+/**
+ * Personal Statistic class.
+ * Takes an incident list and finds the year with the most incidences within that list
+ * @author Aakash Doshi
+ *
+ */
 public class YearWithMostIncidents {
 	
 	private IncidentsFetcher fetcher;
@@ -11,28 +17,41 @@ public class YearWithMostIncidents {
 	private int yearWithMostIncidents;
 	private int counter;
 	
+	/**
+	 * Constructor. initialises fetcher
+	 * @param fetcher
+	 */
 	public YearWithMostIncidents(IncidentsFetcher fetcher) {
 		this.fetcher = fetcher;	
 	}
 	
+	/*
+	 * method to update yearWithMostIncidents
+	 */
 	public void updateYearWithMostIncidents(){
+		//resets counter and yearWithMostIncidences
 		counter = 0;
         yearWithMostIncidents = 0;
 		
+        //updates incident list
         incidentsList = fetcher.getIncidentsList();
         
+        //for each year, goes through all incidents in incident list and increments a tempcounter if the incident year matches
 		for (int i = fetcher.getStartDate(); i < fetcher.getEndDate()+1; i++){
 			int currentYear = i;
 			int tempCounter = 0;
 			
+			//goes through all incidents in incidentlist
 			for (Incident incident : incidentsList){
 				String yearString = incident.getDateAndTime().substring(0, 4);
 				int incidentYear = Integer.parseInt(yearString);
+				//if incident year is the same as current year increments temp counter
 				if(incidentYear == currentYear){
 					tempCounter++;
 				}
 			}
 			
+			//if temp counter is greater than counter update yearWithMostIncidences and counter
 			if (tempCounter > counter){
 				counter = tempCounter;
 				yearWithMostIncidents = currentYear;
@@ -41,8 +60,13 @@ public class YearWithMostIncidents {
 	}
 	
 	
-	
+	/**
+	 * getter method for yearWithMostIncidences with a catch for invalid results
+	 * @return
+	 */
 	public String getYearWithMostIncidents(){
+		//if yearWithMostIncidences is 0, incidentList is probably null thus return "-"
+		//else return the year with most incidences
 		if (yearWithMostIncidents == 0){
 			return "-";
 		}else{
--- ppa-major-coursework/src/statistics/view/StatisticsPanel.java
+++ ppa-major-coursework/src/statistics/view/StatisticsPanel.java
@@ -19,6 +19,20 @@ import model.IncidentsFetcher;
 import statistics.model.StatisticsPreferenceManager;
 import statistics.model.StatsModel;
 
+
+/**
+ * This class is quite confusing but bear with me
+ * 
+ * The main Statistic Panel has inside it 4 sub panels that all act the same way.
+ * The sub panels have two JLabels for a title and the actual statistic as well as two buttons to change them.
+ * The main panel has two arrays 8 elements big, for the titles and the statistics
+ * The sub panels work by having a "position" that corresponds with the index on the title and statistic arrays
+ * They display the statistic and title by getting them from the arrays in the same index as their position
+ * They change position in such a way that no two panels can have the same position
+ * This is done by looking to another array of bools that say if that position is occupied or not
+ * 
+ * @author Aakash
+ */
 public class StatisticsPanel extends JPanel implements Observer {
 	
 	private StatisticsSubPanel subPanel0;
@@ -31,19 +45,11 @@ public class StatisticsPanel extends JPanel implements Observer {
 	private StatsModel model;
 	private StatisticsPreferenceManager panelPref;
 	
-	//This class is quite confusing but bear with me
 	
-	/* The main Statistic Panel has inside it 4 sub panels that all act the same way
-	 * The sub panels have two JLabels for a title and the actual statistic as well as buttons to change them
-	 * The main panel has two arrays 8 elements big for the title and the statistic
-	 * The subpanels work by having a "position" that corresponds with the index on the title and statistic array
-	 * They display the statistic and title by getting them from the arrays in the same index as their position
-	 * They change position in such a way that no two panels can have the same position
-	 * This is done by looking to another array of bools that say if that position is occupied or not
-	 *
-	 * */
-	
-	
+	/**
+	 * Constructor
+	 * @param IncidentsFetcher
+	 */
 	public StatisticsPanel(IncidentsFetcher iFetcher){
 		super();	
 		
@@ -54,7 +60,7 @@ public class StatisticsPanel extends JPanel implements Observer {
 		titles[0] = "Hoaxes";
 		titles[1] = "Non US Sightings";
 		titles[2] = "Next Likeliest State";
-		titles[3] = "youtbe UFO videos";
+		titles[3] = "Youtube UFO videos";
 		titles[4] = "Year with most incidents";
 		titles[5] = "Most Common UFO shape";
 		titles[6] = "Total Number Of Incidents";
@@ -73,8 +79,12 @@ public class StatisticsPanel extends JPanel implements Observer {
 		initMainPanelWidgets();
 	}
 	
+	/**
+	 * initialises widgets for the main Statistics Panel
+	 */
 	public void initMainPanelWidgets(){
 		
+		//manages preferences for the subpanels
 		panelPref = new StatisticsPreferenceManager();
 		
 		//creating sub panels
@@ -97,7 +107,13 @@ public class StatisticsPanel extends JPanel implements Observer {
 
 	/*---------------------------------Start of sub panel class------------------------------------*/
 	
-	//Sub panel class
+	/**
+	 * Subpanel Class that consists of two jlabels (one for title and one for the statistic)
+	 * as well as two buttons for moving to the next or previous statistic that isn't already being shown
+	 * 
+	 * @author Aakash
+	 *
+	 */
 	public class StatisticsSubPanel extends JPanel {
 
 		private JLabel title;
@@ -105,11 +121,16 @@ public class StatisticsPanel extends JPanel implements Observer {
 		private int position;
 		private int panelNumber;
 		
-		//Constructor
-		StatisticsSubPanel() {
+		/**
+		 * Constructor when no panel number has been given
+		 * Takes the first untaken position
+		 * @param panelNumber
+		 */
+		StatisticsSubPanel(int panelNumber) {
 			super();
 			initSubPanelWidgets();
 			setBorder(new EtchedBorder(EtchedBorder.RAISED));
+			this.panelNumber = panelNumber;
 			
 			//takes the first unoccupied position
 			for (int i = 0; i < positionBools.length ; i ++){
@@ -121,6 +142,12 @@ public class StatisticsPanel extends JPanel implements Observer {
 			}
 		}
 		
+		/**
+		 * Constructor to put panels in specific positions. 
+		 * Useful for when you want to save preferences
+		 * @param index
+		 * @param panelNumber
+		 */
 		StatisticsSubPanel(int index, int panelNumber){
 			super();
 			initSubPanelWidgets();
@@ -131,11 +158,13 @@ public class StatisticsPanel extends JPanel implements Observer {
 		}
 		
 
+		/**
+		 * initialises widges for the subpanel and adds listeners
+		 */
 		private void initSubPanelWidgets() {
 			//button stuff
 			JButton leftButton = new JButton("<");
 			JButton rightButton = new JButton(">");
-
 			Font buttonFont = new Font("Dialog", Font.BOLD, 30);
 			leftButton.setFont(buttonFont);
 			rightButton.setFont(buttonFont);
@@ -157,47 +186,71 @@ public class StatisticsPanel extends JPanel implements Observer {
 			title.setFont(new Font("Dialog", Font.BOLD, 22));
 			statistic.setFont(new Font("Dialog", Font.BOLD, 44));
 			
+			//adds labels
 			this.add(title, BorderLayout.NORTH);
 			this.add(statistic, BorderLayout.CENTER);
 
 			this.setMinimumSize(new Dimension(300, 300));
 		}
 		
+		/**
+		 * updates label text with newStat
+		 * @param newStat
+		 */
 		private void setStat(String newStat){
 			statistic.setText(newStat);
 		}
 		
-		//updates title and statistic according to new position
-		//also updates the boolean array to occupy new position
+		/**
+		 * updates title and statistic according to new position
+		 * also updates the boolean array to occupy new position
+		 * @param index
+		 */
+
 		public void setStatisticPosition(int index){
+			//sets current position as unoccupied
 			positionBools[position] = false;
+			//updates title and statistic
 			title.setText(titles[index]);
 			setStat(stats[index]);
+			//sets new position as occupied
 			positionBools[index] = true;
+			//updates position variable
 			position = index;
 			this.revalidate();
 			this.repaint();
 		}
 		
-		//moved the panel to the next unoccupied position to the right. 
-		//Rolls over to start if no unoccupied positions to the right
+		/**
+		 * moved the panel to the next unoccupied position to the right. 
+		 * Rolls over to start if no unoccupied positions to the right.
+		 */
 		public void moveRight(){
 			int newPosition = position;
 			while(positionBools[newPosition]){
+				//formula to find to next position including rollover
 				newPosition = (newPosition + 1) % 8;
 			}
 			
+			//sets new position
 			setStatisticPosition(newPosition);
+			//saves preference
 			panelPref.replacePreferance(panelNumber, newPosition);
 		}
 		
-		//same as moveRight but towards the left
+		/**
+		 * moved the panel to the next unoccupied position to the left. 
+		 * Rolls over to start if no unoccupied positions to the left.
+		 */
 		public void moveLeft(){
 			int newPosition = position;
 			while(positionBools[newPosition]){
+				//formula to find to next position including rollover
 				newPosition = (((newPosition - 1)%8)+8)%8;
 			}
+			//sets new position
 			setStatisticPosition(newPosition);
+			//saves preference
 			panelPref.replacePreferance(panelNumber, newPosition);
 		}
 		
@@ -205,16 +258,28 @@ public class StatisticsPanel extends JPanel implements Observer {
 			return position;
 		}
 		
-		//Listener class for the buttons
+		/**
+		 * Listener class for the subpanel buttons
+		 * @author Aakash
+		 *
+		 */
 		public class StatsButtonListeners implements ActionListener{
 
-			//goRight checks if the button is a button to go right or go left
+			//boolean goRight checks if the button is a button to go right or go left
 			boolean goRight;
+			
+			/**
+			 * Constructor
+			 * param boolean goRight checks if the button is a button to go right or go left
+			 * @param goRight
+			 */
 			public StatsButtonListeners(boolean goRight) {
 				this.goRight = goRight;
 			}
 			
-			//calls move methods depending on if the button goes right or left
+			/**
+			 * calls move methods depending on if the button goes right or left
+			 */
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				if(goRight){
@@ -229,7 +294,9 @@ public class StatisticsPanel extends JPanel implements Observer {
 	/*------------------------------------End of sub panel class-------------------------------------*/
 	
 
-	
+	/**
+	 * updated statistics array and subpanel statistics
+	 */
 	@Override
 	public void update(Observable o, Object arg) {
 		//updates statistics values in the statistics array
