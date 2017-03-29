4b8db5667faae957ce10cb97b527cffd97e32ee2
Aakash <k1630608@kcl.ac.uk>
2017-03-29 20:26:27 +0100

    finished comments for statistics.model package

--- ppa-major-coursework/src/statistics/model/StatisticsPreferenceManager.java
+++ ppa-major-coursework/src/statistics/model/StatisticsPreferenceManager.java
@@ -5,6 +5,11 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
+/**
+ * class to manage the prefered positions for each of the statistics in the statistics panel
+ * @author Aakash
+ *
+ */
 public class StatisticsPreferenceManager {
 
 	private List<String> preferenceList;
@@ -14,11 +19,20 @@ public class StatisticsPreferenceManager {
 	private int thirdPref;
 	private int fourthPref;
 	
+	/**
+	 * Constructor
+	 */
 	public StatisticsPreferenceManager(){
 		readFiles();
 	}
 	
+	/**
+	 * reads files and puts each line in a list
+	 * sets preference variables based on what's in the  list
+	 *
+	 */
 	private void readFiles(){
+		//put file contents in a list
 		Path path = Paths.get(FILE_PATH);
 		try {
 			preferenceList = Files.readAllLines(path);
@@ -26,6 +40,8 @@ public class StatisticsPreferenceManager {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		//set pref variables
 		firstPref = Integer.parseInt(preferenceList.get(0));
 		secondPref = Integer.parseInt(preferenceList.get(1));
 		thirdPref = Integer.parseInt(preferenceList.get(2));
@@ -33,9 +49,18 @@ public class StatisticsPreferenceManager {
 		
 	}
 	
-	public void replacePreferance(int panelNumber, int newPreferance){
+	/**
+	 * replaces the "panelNumber" line in the file with "newPreferance"
+	 * @param panelNumber
+	 * @param newPreferance
+	 */
+	public void replacePreferance(int panelNumber, int newPreference){
+		//removes panelNumber line from the list
 		preferenceList.remove(panelNumber);
-		preferenceList.add(panelNumber, new Integer(newPreferance).toString());
+		//adds newPreference to the panelNumber index
+		preferenceList.add(panelNumber, new Integer(newPreference).toString());
+		
+		//writes the preference list to the file
 		Path path = Paths.get(FILE_PATH);
 	    try {
 			Files.write(path, preferenceList);
@@ -46,19 +71,34 @@ public class StatisticsPreferenceManager {
 	    
 	}
 	
-	
+	/**
+	 * getter method for first preference 
+	 * @return firstPref
+	 */
 	public int getFirstPreference(){
 		return firstPref;
 	}
 	
+	/**
+	 * getter method for second preference 
+	 * @return secondPref
+	 */
 	public int getSecondPreference(){
 		return secondPref;
 	}
 	
+	/**
+	 * getter method for third preference 
+	 * @return thirdPref
+	 */
 	public int getThirdPreference(){
 		return thirdPref;
 	}
 	
+	/**
+	 * getter method for fourth preference 
+	 * @return fourthPref
+	 */
 	public int getFourthPreference(){
 		return fourthPref;
 	}
